5f4fbbc412a2d1ffac4f1590f4f46f3cd9bbcdd1
nicholas-yong <40938341+nicholas-yong@users.noreply.github.com>
2021-08-07 20:32:33 +0800

    Translate Project
    As per title.

---
+++ translate/src/components/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Button extends React.Component{
+    render()
+    {
+        return (
+        <button className = "ui button primary">
+            Submit
+        </button>
+        );
+    }
+}
+
+export default Button
